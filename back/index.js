7ef0e299a7fa1c0bd2d479f2e675a5f3a8aebd3d
Persephone-ms <fabiamsfabia@gmail.com>
2026-01-20 23:58:35 +0000

    add

---
+++ back/index.js
@@ -0,0 +1,14 @@
+console.log("Olá mundo")
+
+function soma1 (){
+    console.log(1 + 1)
+}
+
+function somaGeral (a, b){
+    console.log(a+b)
+}
+
+somaGeral (2,0)
+somaGeral (1,8)
+somaGeral (8,2)
+somaGeral (7,8)
